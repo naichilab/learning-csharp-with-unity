51b4a6a68e0d3835cb4913ddaedd45ed17a1c3ff
naichilab <naichilab@live.jp>
2017-07-10 03:28:47 +0900

    edit

--- Assets/Sample_00/Sample.cs
+++ Assets/Sample_00/Sample.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Sample : MonoBehaviour
+{
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		
 	}
 }
