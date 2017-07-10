a109029c6b51aa90412a38d5e7527fc8207ffb34
naichilab <naichilab@live.jp>
2017-07-11 02:05:17 +0900

    edit

---
+++ Assets/comment/CommentSample.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommentSample : MonoBehaviour
+{
+	void Start ()
+	{
+		//この行はコメントです。
+		//プログラムの補足説明を書いたりします。
+		Debug.Log ("こんにちは");
+	}
+}
